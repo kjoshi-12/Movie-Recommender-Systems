150ab018cf7e805fc9bea23aa028691e38e59d08
SeveralCube22 <manam.viswadeep@gmail.com>
2021-05-02 23:10:02 -0700

    created function to determine vectors

--- Collaborative-Based Filtering/collaborative_filter.py
+++ Collaborative-Based Filtering/collaborative_filter.py
@@ -1,4 +1,6 @@
 import csv
+from scipy import spatial
+
 
 def readMovies():
     movies = {}
@@ -21,7 +23,7 @@ def readUserRatings(movies):
     with open("ratings_small.csv", encoding="UTF-8") as csvFile:
         reader = csv.reader(csvFile)
         lines = 0
-        ratedMovies = []
+        ratedMovies = {}
         currentUser = 1
         numUsers = 0
         numIgnored = 0
@@ -33,13 +35,13 @@ def readUserRatings(movies):
                 userId, movieId, movieRating = int(row[0]), int(row[1]), float(row[2])
                 if userId != currentUser:
                     userRatings[currentUser] = ratedMovies
-                    ratedMovies = []
+                    ratedMovies = {}
                     currentUser = userId
                     numUsers += 1
 
                 try:
                     movieName = movies[movieId] # check if movieId exists
-                    ratedMovies.append([movieId, movieRating])
+                    ratedMovies[movieId] = movieRating
                 except KeyError:
                     numIgnored += 1 # certain movie ids are in ratings_small.csv, but not in movies_metadata.csv, so just ignore these ids
 
@@ -48,7 +50,52 @@ def readUserRatings(movies):
 
     return userRatings, numUsers
 
+def determineVectors(ratings1, ratings2):
+    user1Movies = list(ratings1.keys())
+    user2Movies = list(ratings2.keys())
+    movies = list(set(user1Movies) | set(user2Movies))
+    user1Ratings = []
+    for id in movies:
+        try:
+            rating = ratings1[id]
+        except KeyError:
+            rating = 0
+        user1Ratings.append(rating)
+
+    user2Ratings = []
+    for id in movies:
+        try:
+            rating = ratings2[id]
+        except KeyError:
+            rating = 0
+        user2Ratings.append(rating)
+    return user1Ratings, user2Ratings
+
+#Cosine Similarity computation: helps in choosing the movies that are most similar to another user's movie taste
+def cosine_similarity(ratings_x, ratings_y):
+    result = 1 - spatial.distance.cosine(ratings_x, ratings_y)
+    return result
+
+
+def generateMatrix(userRatings, numUsers):
+    matrix = []
+    for i in range(1, numUsers + 1):
+        similarity = []
+        for j in range(i + 1, numUsers + 1):
+            ratings1 = userRatings[i]
+            ratings2 = userRatings[j]
+            user1Ratings, user2Ratings = determineVectors(ratings1, ratings2)
+
+            sim = cosine_similarity(user1Ratings, user2Ratings)
+            similarity.append(sim)
+        matrix.append(similarity)
+    return matrix
 
 movies, numMovies = readMovies()
 userRatings, numUsers = readUserRatings(movies)
+
+matrix = generateMatrix(userRatings, numUsers)
+
+print(matrix[0])
+
 
