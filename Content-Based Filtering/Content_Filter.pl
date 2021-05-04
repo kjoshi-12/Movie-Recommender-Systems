ec573b20f63f840590e2cf3dc2585ec258177ad0
Adrianj0902 <82130614+Adrianj0902@users.noreply.github.com>
2021-05-04 09:54:29 -0700

    Content_Filter.pl
     prolog program for content based filtering

---
+++ Content-Based Filtering/Content_Filter.pl
@@ -0,0 +1,291 @@
+
+% Knowledge bases/Rules
+% struct for data film(title,genre,actors,rating)
+
+
+% 50 comedy movies-----------------------------------
+film('Friday', comedy,'Ice Cube','Chris Tucker','R').
+film('Home for the Holidays',comedy,'Anne Bancroft', 'Robert Downey Jr','PG-13').
+film('Grumpier Old Men',comedy,'Jack Lemmon', 'Walter Matthau','PG-13').
+film('The American President',comedy,'Michael Douglas', 'Annette Bening','PG-13').
+film('It Takes Two',comedy,'Kirstie Alley','Steve Guttenberg','PG').
+film('To Die For',comedy, 'Nicole Kidman', 'Matt Dillon','R').
+film('Mighty Aphrodite',comedy,'Woody Allen', 'Mira Sorvino','R').
+film('The Big Green',comedy,'Steve Guttenberg', 'Olivia d Abo','PG').
+film('Dont Be a Menace to South Central While Drinking Your Juice in the Hood',comedy,'Shawn Wayans', 'Marlon Wayans','R').
+film('Bio-Dome',comedy,'Pauly Shore', 'Stephen Baldwin','PG-13').
+
+film('Parasite ', comedy,'Kang-ho Song,','Sun-kyun Lee','R').
+film('Life Is Beautiful',comedy,' Roberto Benigni', 'Nicoletta Braschi,','PG-13').
+film('The Intouchables ',comedy,'François Cluzet', 'Omar Sy','R').
+film('Back To The Future',comedy,'Michael J Fox', 'Christopher Llody','PG').
+film('It Takes Two',comedy,'Kirstie Alley','Steve Guttenberg','PG').
+film('Modern Times',comedy, 'Charles Chaplin', 'Paulette Goddard','G').
+film('City Lights',comedy,'Charles Chaplin', 'Virginia Cherrill','G').
+film('3 Idiots',comedy,'Aamir Khan', 'Mona Singh','PG-13').
+film('The Great Dictator',comedy,'Charles Chaplin', 'Paulette Goddard','G').
+film('Snatch',comedy,'Jason Statham', 'Brad Pitt','R').
+
+film('The Sting', comedy,'Paul Newman,','Robert Redford','PG').
+film('The Apartment',comedy,'Jack Lemmon', 'Shirley MacLaine','PG-13').
+film('Singin In The Rain ',comedy,'Gene Kelly', 'Donald O Connor','G').
+film('The Kid',comedy,'Charles Chaplin', 'Edna Purviance','PG').
+film('Green Book',comedy,'Viggo Mortensen', 'Mahershala Ali' ,'PG-13').
+film('Klaus',comedy,'Jason Schwartzman', 'J.K. Simmons','PG').
+film('Queen',comedy, 'Kangana Ranaut', 'Rajkummar Rao','G').
+film('The Gold Rush',comedy,'Charles Chaplin', 'Mack Swain','PG-13').
+film('Sherlock Jr.',comedy,'Buster Keaton', 'Kathryn McGuire','G').
+film('Wild Tails',comedy,'Darío Grandinetti', 'María Marull','R').
+
+film('PK', comedy,'Aamir Khan', 'Anushka Sharma','PG').
+film('OMG: Oh My God!',comedy,'Paresh Rawal', 'Akshay Kumar','PG-13').
+film('The Grand Budapest Hotel',comedy,'Ralph Fiennes', 'F. Murray Abraham','G').
+film('Bafi',comedy,'Ranbir Kapoor', 'Priyanka Chopra','PG').
+film('Mary And Max',comedy,'Toni Collette', 'Philip Seymour Hoffman','PG-13').
+film('Black Cat, White Cat',comedy,'Bajram Severdzan', 'Srdjan Zika Todorovic','R').
+film('The Big Lebowski',comedy, 'Jeff Bridges', 'John Goodman','G').
+film('Underground',comedy,'Predrag Miki Manojlovic', 'Lazar Ristovski','PG-13').
+film('Chungking Express',comedy,'Brigitte Lin', 'Takeshi Kaneshiro','PG-13').
+film('Dead Poet Society',comedy,'Robin Williams', 'Robert Sean Leonard','PG').
+
+film('Monty Python Life of Brian', comedy, 'Graham Chapman', 'John Cleese','R').
+film('Paper Moon',comedy,'Ryan ONeal', 'Tatum ONeal','PG').
+film('The Shop Around The Corner',comedy, 'Margaret Sullavan', 'James Stewart','PG-13').
+film('Mr.Smith Goes To Washinton',comedy,'James Stewart', 'Jean Arthur','PG').
+film('It Happened One Night',comedy,'Clark Gable', 'Claudette Colbert','PG-13').
+film('The Circus',comedy,'Charles Chaplin', 'Merna Kennedy','PG-13').
+film('The General',comedy, 'Clyde Bruckman', 'Buster Keaton' ,'G').
+film('Groundhog Day',comedy,'Bill Murray', 'Andie MacDowell','PG').
+film('Do The Right Thing',comedy,'Danny Aiello', 'Ossie Davis','R').
+film('Being There',comedy,'Peter Sellers', 'Shirley MacLaine','PG').
+%-----------------------------------------------------------------
+
+% 50 crime movies-------------------------------------------------
+film('Heat',crime, 'Al Pacino', 'Robert De Niro','R').
+film('Casino',crime, 'Robert De Niro', 'Joe Pesci','R').
+film('Four Rooms',crime, 'Tim Roth', 'Antonio Banderas','R').
+film('Ace Ventura: When Nature Calls',crime,'Jim Carrey', 'Ian McNeice','PG-13').
+film('Money Train',crime,'Wesley Snipes', 'Woody Harrelson','R').
+film('Get Shorty',crime, 'Gene Hackman', 'Rene Russo','R').
+film('Assassins',crime, 'Sylvester Stallone', 'Antonio Banderas','R').
+film('Dangerous Minds',crime, 'Michelle Pfeiffer', 'George Dzundza','R').
+film('Dead Presidents',crime,'Keith David', 'Chris Tucker','R').
+film('Se7en',crime,'Morgan Freeman', 'Brad Pitt','R').
+
+film('Promising Young Women',crime,'Carey Mulligan', 'Bo Burnham','R').
+film('Nobody',crime, 'Bob Odenkirk', 'Aleksey Serebryakov','R').
+film('The Godfather',crime, 'Marlon Brando', 'Al Pachino','R').
+film('F9',crime,'Vin Diesal', 'Michelle Rodriguez','PG-13').
+film('Knives Out',crime,'Daniel Craig', 'Chris Evans','PG-13').
+film('The Little Things',crime, 'Denzel Washinton', 'Rami Malek','R').
+film('Sleepers',crime, 'Robert De Niro', 'Kevin Bacon','R').
+film('Joker(2019)',crime, 'Joaquin Phoenix', 'Robert De Niro','R').
+film('The Dark Knight',crime,'Christian Bale', 'Heath Ledge','PG-13').
+film('Fargo',crime,'William H. Macy', 'Frances McDormand','R').
+
+film('Pulp Fiction',crime,'John Travolta', 'Uma Thurman','R').
+film('The Wolf of Wall Street',crime, 'Leonardo DiCaprio', 'Johan Hill','R').
+film('The Silence of the Lambs',crime, 'Anthony Hopkins', 'Jodie Foster','R').
+film('I See You',crime,'Helen Hunt', 'Jon Tenney','R').
+film('The Gentlemen',crime,'Matthew McConaughey', 'Charile Hunna','R').
+film('No Country for Old Men',crime, 'Tommy Lee Jones', 'Javier Bardem','R').
+film('The Green Mile',crime, 'Tom Hanks', 'Bonnie Hunt','R').
+film('The Departed',crime, 'Leonardo DiCaprio', 'Matt Damon','R').
+film('American Me',crime,'Edwards James Olmos', 'William Forsythe','R').
+film('Goodfellas',crime,'Roberto De Niro', 'Ray Liotta','R').
+
+film('John Wick',crime,'Keanu Reeves', 'Michael Nyqvist','R').
+film('John Wick:Chapter 2',crime, 'Keanu Reeves', 'Ruby Rose','R').
+film('John Wick:Chapter 3 Parabellum',crime, 'Keanu Reeves', 'Halle Berry','R').
+film('The Fast and the Furious',crime,'Vin Diesel', 'Paul Walker','PG-13').
+film('Scarface',crime,'Al Pacino', 'Michelle Pfeiffer','R').
+film('Baby Driver',crime, 'Ansel Elgort', 'Jon Bernthal','R').
+film('The Fate of the Furious',crime, 'Vin Diesel', 'Jason Statham','PG-13').
+film('The Hateful Eight',crime, 'Samuel L. Jackson', 'Kurt Russell','R').
+film('The Irishman',crime,'Robert De Niro', 'Al Pacino','R').
+film('The Outsiders',crime,'Matt Dillon', 'Ralph Macchio','PG').
+
+
+film('Prisoners',crime,'Hugh Jackman', 'Jake Gyllenhaal','R').
+film('Honest Theif',crime, 'Liam Nelson', 'Kate Walsh','PG-13').
+film('Nightcrawler',crime, 'Jake Gyllenhaal', 'Rene Russo','R').
+film('The Informer',crime,'Joel Kinnaman','Rosamund Pike','R').
+film('Drive',crime,'Ryan Gosling', 'Carey Mulligan','R').
+film('The Usual Suspects',crime, 'Kevin Spacey', 'Gabriel Byrne','R').
+film('Baywatch(2017)',crime, 'Dwayne Johnson','Zac Efron','R').
+film('My Cousin Vinny',crime, 'Joe Pesci', 'Marisa Tomei','R').
+film('Kill Bill: Vol. 1',crime,'Uma Thurman', 'David Carradine','R').
+film('Just Mercy',crime,'Michael B Jordan', 'Jamie Foxx','PG-13').
+%-----------------------------------------------------------------
+
+% 50 drama movies
+film('Othello',drama,'Laurence Fishburne', 'Kenneth Branagh','R').
+film('Persuasion',drama,'Sally Hawkins', 'Alice Krige','PG').
+film('Dead Man Walking',drama,'Susan Sarandon', 'Sean Penn','R').
+film('Argo',drama,'Ben Affleck', 'Bryan Cranston','R').
+film('Cry, he Beloved Country',drama, 'Richard Harris', 'James Earl Jones','R').
+film('Restoration',drama,'Robert Downey Jr.','Sam Neill','R').
+film('Georgia',drama,'Jennifer Jason Leigh', 'Mare Winningham','R').
+film('The Journey of August King',drama, 'Jason Patric', 'Thandiwe Newton','PG-13').
+film('The Boys of St. Vincent',drama, 'Henry Czerny', 'Johnny Morina','R').
+film('Apollo 13',drama, 'Tom Hanks', 'Bill Paxton', 'PG').
+
+film('Solace',drama,'Anthony Hopkins', 'Jeffrey Dean Morgan','R').
+film('Inside Man',drama,'Denzel Washinton', 'Clive Owen','R').
+film('The Place Beyond the Pines',drama,'Ryan Gosling', 'Bradley Cooper','R').
+film('The House That Jack Built',drama,'Matt Dillon', 'Bruno Ganz','R').
+film('Carlitos Way',drama, 'Al Pacino', 'Sean Penn','R').
+film('The Crying Game',drama,'Stephan Rea','Jaye Davidson','R').
+film('The Accountant',drama,'Ben Affleck', 'Anna Kendrick','R').
+film('Logan Lucky',drama, 'Channing Tatum', 'Adam Driver','PG-13').
+film('The Mule',drama, 'Clint Eastwood', 'Patrick L Reyes','R').
+film('Good Time',drama, 'Robert Pattinson', 'Benny Safdie', 'R').
+
+
+film('Risky Business',drama,'Tom Cruise', 'Rebecca De Mornay','R').
+film('Blood Simple',drama,'John Getz', 'Frances McDormand','R').
+film('War Dogs',drama,'Jonah Hill', 'Miles Teller','R').
+film('Primal Fear',drama,'Richard Gere', 'Laura Linney','R').
+film('The Fugitive',drama, 'Harrison Ford', 'Tommy Lee Jones','PG-13').
+film('Monster',drama,'Charlize Theron','Christina Rucci','R').
+film('Shot Caller',drama,'Nikolaj Coster-Waldau', 'Omari Hardwick','R').
+film('Sky High',drama, 'Miguel Herran', 'Carolina Yuste','R').
+film('The Drop',drama, 'Tom Hardy', 'Noomi Rapace','R').
+film('Queen & Slim',drama, 'Daniel Kaluuya', 'Jodie Turner-Smith', 'R').
+
+
+film('Smokin Aces',drama,'Jeremy Piven', 'Ryan Reynolds','R').
+film('Law Abiding Citizen',drama,'Gerard Butler', 'Jamie Foxx','R').
+film('Dog Day Afternoon',drama,'Al Pacino', 'John Cazale','R').
+film('The Lincon Lawyer',drama,'Matthew McConaughey', 'Marisa Tomei','R').
+film('The Tax Collector',drama, 'Bobby Soto', 'Shia LaBeouf','R').
+film('Jackie Brown',drama,'Pam Grier','Samuel L Jackson','R').
+film('The Big Bull',drama,'Abhishek Bachchan', 'Nikita Dutta','R').
+film('The Girl Next Door',drama, 'William Atherton', 'Blythe Auffarth','R').
+film('Hell or High Water(II)',drama, 'Chris Pine', 'Ben Foster','R').
+film('Wyatt Earp',drama, 'Kevin Costner', 'Dennis Quaid', 'PG-13').
+
+film('City of Lies',drama,'Johnny Depp', 'Forest Whitaker','R').
+film('Gone Baby Gone',drama,'Ami Ameen', 'Red Madrell','R').
+film('Richard Jewell',drama,'Paul Walter Hauser', 'Sam Rockwell','R').
+film('Falling Down',drama,'Michael Douglas', 'Robert Duvall','R').
+film('The Untouchables',drama, 'Kevin Costner', 'Robert De Niro','R').
+film('Road to Perdition',drama,'Tom Hanks','Tyler Hoechlin','R').
+film('Papillon',drama,'Steve Mcqueen', 'Dustin Hoffman','R').
+film('Boyz n the Hood',drama, 'Cuba Gooding Jr.', 'Laurence Fishburne','R').
+film('Clear and Present Danger',drama, 'Harrison Ford', 'Willem Dafoe','PG-13').
+film('Sicario: Day of the Soldado',drama, 'Benicio Del Toro', 'Josh Brolin', 'PG-13').
+
+
+% 50 family movies ---------------------------------------------
+film('Toy Story Collection',family, 'Tim Allen', 'Tom Hanks','G').
+film('Jumanji', family,'Robin Williams', 'Kristen Dunst','PG').
+film('Big Bully',family,'Rick Moranis', 'Tom Arnold','PG').
+film('Casper',family,'Bill Pullman', 'Christina Ricci','PG').
+film('A Goofy Movie',family,'Bill Farmer', 'Jason Marsden','G').
+film('The Flintstones',family,'John Goodman', 'Rick Moranis','PG').
+film('The Jungle Book',family,'Jason Scott Lee', 'Cary Elwes','PG').
+film('The Lion King',family,'James Earl Jones', 'Matthew Broderick','G').
+film('Coneheads',family,'Dan Aykroyd', 'Jane Curtin','PG').
+film('Super Mario Bros',family,'Bob Hoskins', 'John Leguizamo','PG').
+
+film('The Mitchells vs. The Machines',family, 'Abbi Jacobson', 'Danny McBride','PG').
+film('Soul', family,'Jamie Foxx', 'Tina Fey','PG').
+film('Pinocchio',family,'Federico Ielapi', 'Roberto Benigni','PG-13').
+film('Raya The Last Dragon',family,'Kelly Marie Tran', 'Awkwafina','PG').
+film('Harry Potter: Collection',family,'Daniel Radcliffe', 'Rupert Grint','PG').
+film('The Mighty Ducks',family,'Emilio Estevez', 'Joss Ackland','PG').
+film('StarDust',family,'Charlie Cox', 'Claire Danes','PG').
+film('The Goonies',family,'Sean Astin', 'Josh Brolin','PG').
+film('Tom & Jerry',family,'Chloë Grace Moretz', 'Michael Peña','PG').
+film('Mulan',family,'Yifei Liu', 'Donnie Yen','PG-13').
+
+film('The Little Rascals',family,'Travis Tedford', 'Bug Hall','PG').
+film('Wolfwalkers', family,'Honor Kneafsey', 'Eva Whittaker','PG').
+film('Pinocchio',family,'Federico Ielapi', 'Roberto Benigni','PG-13').
+film('The Princess Bride',family,'Cary Elwes', 'Mandy Patinkin','PG').
+film('The Sandlot',family,'Tom Guiry', 'Mike Vitar','PG').
+film('Spirited Away',family,'Daveigh Chase', 'Suzanne Pleshette','PG').
+film('Mary Poppins',family,'Julie Andrews', 'Dick Van Dyke','G').
+film('Yes Day',family,'Jennifer Garner', 'Edgar Ramirez','PG').
+film('Better Days',family,'Dongyu Zhou', 'Jackson Lee','PG').
+film('Coco',family,'Anthony Gonzalez', 'Gael Garcia Bernal','PG').
+
+film('Cars',family,'Owen Wilson', 'Bonnie Hunt','G').
+film('D2: The Mighty Ducks', family,'Emilio Estevez', 'Kathryn Erbe','PG').
+film('Wonder',family,'Jacob Tremblay', 'Owen Wilson','PG').
+film('The Croods: A New Age',family,'Nicolas Cage', 'Emma Stone','PG').
+film('Hook',family,'Dustin Hoffman', 'Robin Williams','PG').
+film('Beauty And The Beast',family,'Emma Watson', 'Dan Stevens','PG').
+film('The Karate Kid(1984)',family,'Ralph Macchio', 'Pat Morita','PG').
+film('Aladdin(2019)',family,'Will Smith', 'Mena Massoud','PG').
+film('The Wizard of OZ',family,'Judy Garland', 'Frank Morgan','PG').
+film('Night at The Museum',family,'Ben Stiller', 'Carla Gugino','PG').
+
+film('Cats',family,'James Corden', 'Judi Dench','PG').
+film('A Week Away', family,'Kevin Quinn', 'Bailee Madison','PG-13').
+film('Matilda',family,'Danny DeVito', 'Rhea Perlman','PG').
+film('The Witches',family,'Anne Hathaway', 'Octavia Spencer','PG').
+film('Charile and the Chocolate Factory',family,'Johnny Depp', 'Freddie Highmore','PG').
+film('Sing',family,'Matthew McConaughey', 'Reese Witherspoon','PG').
+film('Hercules(1997)',family,'Tate Donovan', 'Susan Egan','G').
+film('Scooby-Doo',family,'Matthew Lillard', 'Freddie Prince Jr.','PG').
+film('Alice in Wonderland',family,'Mia Wasikowska', 'Johnny Deep','PG').
+film('E.T. the Extra-Terrestrial',family,'Emilio Estevez', 'Drew Berrymore','PG').
+
+% a menu with multiple options to filter movies based
+% on certain content such as genre, authors,rating, or combination of
+% options.
+
+start:- write('*Welcome To Our Movie Recommender*'),nl,nl,
+write('Select 1 to view all movies related by genre'),nl,
+write('Select 2 to view all movies that are simlilar based on a rating'),nl,
+write('Select 3 to view all movies that are similar based on a actor/actress'),nl,
+write('Select 4 to view all movies that the contain the same Actor/Actress & Rating'),nl,
+write('Select 5 to view all movies that the contain the same Actor/Actress & Genre'),nl,
+write('Select 6 to view all movies that the contain the same Genre & Rating'),nl,
+write(' '), read(X), option(X).
+
+% print movies with same genre based on user choice
+
+option(1):-write('Select from the following Genres (family, crime, comedy, drama)'),nl,
+read(X),forall(film(Y,X,_,_,_),writeln(Y)),again.
+
+
+% print movies based on ratings
+
+option(2):- write('Enter a Rating PG, PG-13, R, or G'),
+nl,nl, read(X),forall(film(Y,_,_,_,X),writeln(Y)),again.
+
+% print movies based on a single actor or actress
+% order of the actors does not matter
+
+option(3) :-write('Enter a Actor/Actress Name'),nl,nl,read(X),
+    forall(film(Y,_,X,_,_);film(Y,_,_,X,_),writeln(Y)),again.
+
+% print movies based on actors/actress & rating
+
+option(4) :-write('Enter the Actor/Actress followed by a Rating'),nl,nl,
+    read(X),read(Y),forall(film(Z,_,X,_,Y);film(Z,_,_,X,Y),writeln(Z)),again.
+% print movies based on actors/actress & genre
+
+option(5):-write('Enter the Actor/Actress followed by a Genre'),nl,nl,
+    read(X),read(Y),forall(film(Z,Y,X,_,_);film(Z,Y,_,X,_),writeln(Z)),again.
+
+% print movies based on genre & rating
+
+option(6):-write('Enter the genre followed by a Rating'),nl,nl,
+    read(X),read(Y),forall(film(Z,X,_,_,Y),writeln(Z)),again.
+
+
+% give the user the choice to make another selection
+% based on a genre,rating,actor/actress,or combination.
+again:- write('Would You Like To Search Again?'),nl,nl,
+    write('Select 1 to view all movies related by genre'),nl,
+    write('Select 2 to view all movies that are simlilar based on a rating'),nl,
+    write('Select 3 to view all movies that are similar based on a actor/actress'),nl,
+    write('Select 4 to view all movies that the contain the same Actor/Actress & Rating'),nl,
+    write('Select 5 to view all movies that the contain the same Actor/Actress & Genre'),nl,
+    write('Select 6 to view all movies that the contain the same Genre & Rating'),nl,
+    write('Select 0 to exit.'),nl,
+    read(X),option(X);false.
